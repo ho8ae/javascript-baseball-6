cc41ecadcc422f1b81159b6646a1722d6bbc1655
ho8ae <xogh2242@gmail.com>
2024-10-01 01:28:45 +0900

    feat: baseball 스트라이크/ 볼 카운트 reduce 와 filter 방식으로 기능 구현

--- src/domain/Baseball.js
+++ src/domain/Baseball.js
@@ -11,27 +11,43 @@ class BaseBall {
 
 
     //input = Array 형태
+    // getStrikeCount(input) {
+    //     let count = 0;
+
+    //     for (let i = 0; i < input.length; i++) {
+    //         if (input[i] === this.#answer[i]) count += 1;
+    //     }
+
+    //     return count;
+    // }
+
     getStrikeCount(input) {
-        let count = 0;
-
-        for (let i = 0; i < input.length; i++) {
-            if (input[i] === this.#answer[i]) count += 1;
-        }
-
+        const count = input.reduce((acc, value, i) => {
+          return value === this.#answer[i] ? acc + 1 : acc;
+        }, 0);
+    
         return count;
-    }
+      }
 
     //input = Array 형태
 
+    // getBallCount(input, strikeCount) {
+    //     let count = 0;
+
+    //     for (let i = 0; i < input.length; i++) {
+    //         if (this.#answer.includes(input[i])) count += 1;
+    //     }
+
+    //     return count - strikeCount;
+    // }
+
     getBallCount(input, strikeCount) {
-        let count = 0;
-
-        for (let i = 0; i < input.length; i++) {
-            if (this.#answer.includes(input[i])) count += 1;
-        }
-
-        return count - strikeCount;
-    }
+        const count =
+          input.filter((value) => this.#answer.includes(value)).length -
+          strikeCount;
+        return count;
+      }
+    
 
     resetGame() {
         this.#answer = RandomNumberGenerator.generateRandomNumber(
