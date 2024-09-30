39b889adafbdf35de2ba5d32af14073c1eccafd3
ho8ae <xogh2242@gmail.com>
2024-09-30 23:24:55 +0900

    feat: 게임 시작 및 힌트 출력 기능 구현

---
+++ src/view/OutputView.mjs
@@ -0,0 +1,26 @@
+import { Console } from "@woowacourse/mission-utils";
+import { GUIDE_MESSAGE } from "../constant/Constant.mjs";
+
+
+const OuputView = {
+    printStartMessage(){
+        Console.print(GUIDE_MESSAGE.startGame);
+    },
+
+    printHintMessage(ballCount, strikeCount){
+        const hint = [];
+        if(ballCount !== 0) hint.push(`${ballCount}볼`);
+        if(strikeCount !== 0) hint.push(`${strikeCount}스트라이크`);
+        if(strikeCount === 0 && strikeCount === 0 ) hint.push(`낫싱`);
+  
+        
+        Console.print(hint.join(" "));
+    },
+
+    printEndMessage(){
+        Console.print(GUIDE_MESSAGE.correctNumber);
+    },
+};
+
+
+export default OuputView;
