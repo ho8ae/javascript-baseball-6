f06b93eee88c38ca9d0319c6297edc1075149d92
ho8ae <xogh2242@gmail.com>
2024-10-01 01:01:29 +0900

    rename: 상수 파일 domain으로 이동 feat: StaticNumber 상수화

--- src/utils/inputValidator.js
+++ src/utils/inputValidator.js
@@ -1,10 +1,10 @@
-import { ERROR_MESSAGE } from "../constant/Constant.js";
+import { ERROR_MESSAGE , STATIC_NUMBER} from "../constant/Constant.js";
 
 const InputValidator = {
     validateUserNumber(input) {
         const inputNumbers = input.split("");
         if (input.length !== 3) throw new Error(ERROR_MESSAGE.inputLengthError);
-        if (input.replace(/[0-9]/g, "").length > 0) throw new Error(ERROR_MESSAGE.inputNumberError);//0-9로 해야 0을 넣었을 때 오류가 나옴
+        if (input.replace(STATIC_NUMBER.possibleBaseballNumber, "").length > 0) throw new Error(ERROR_MESSAGE.inputNumberError);//0-9로 해야 0을 넣었을 때 오류가 나옴
         if (inputNumbers.length !== new Set(inputNumbers).size)
             throw new Error(ERROR_MESSAGE.inputDuplicateError); // 다른 숫자여야 합니다.
         if (inputNumbers.includes("0"))
@@ -13,9 +13,9 @@ const InputValidator = {
 
 
     validateRestartNumber(input) {
-        if (input.length !== 1) throw new Error(ERROR_MESSAGE.restartCountError);
+        if (input.length !== STATIC_NUMBER.restartNumberLength) throw new Error(ERROR_MESSAGE.restartCountError);
         //길이가 1인지 검사
-        if (input.replace(/1|2/g, "").length > 0)
+        if (input.replace(STATIC_NUMBER.possibleEndOrNotNumber, "").length > 0)
             throw new Error(ERROR_MESSAGE.restartNumberError);
         //1또는 2를 검사
     },
