cd53f3003dabd55a9268ae728c1eb81ccb29154c
ho8ae <xogh2242@gmail.com>
2024-09-30 23:16:29 +0900

    feat: 사용자에게 입력 받는 기능 구현

--- src/Test.js
+++ src/Test.js
@@ -1,5 +1,11 @@
+import InputView from "./view/InputView.mjs";
 import InputValidator from "./utils/inputValidator.mjs";
 
 
+InputView.readUserNumber((input)=>{
+    InputValidator.validateUserNumber(input); //검사 실시
+    console.log(input)
+})
 
-console.log(InputValidator.validateRestartNumber("2"));
+//test시 터미널에 node 파일명 
+//mjs 파일이 아니라면 ex) node src/Test.js 이렇게 하면 테스트 가능
