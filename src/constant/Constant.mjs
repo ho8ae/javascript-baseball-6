5a169d23b08811cb4e6d546d34874b67e6a989e6
ho8ae <xogh2242@gmail.com>
2024-10-01 00:07:55 +0900

    refactor: 힌트 출력문구 수정

--- src/constant/Constant.mjs
+++ src/constant/Constant.mjs
@@ -1,6 +1,6 @@
 const GUIDE_MESSAGE = Object.freeze({ //freeze 이해 필요
     startGame: "숫자 야구 게임을 시작합니다.",
-    restartGame: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요",
+    restartGame: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요\n ",
     inputNumber: "숫자를 입력해주세요",
     correctNumber: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
 });
